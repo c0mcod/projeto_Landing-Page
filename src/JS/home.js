cc2cb98f051a3bedf4d2b7b5fafc423c17e5d117
comcod <netosilvaneto6@gmail.com>
2024-12-31 10:52:49 -0300

    :sparkles: feat: Seção de Projetos

--- src/JS/home.js
+++ src/JS/home.js
@@ -2,3 +2,17 @@ document.getElementById('menu-toggle').addEventListener('click', function () {
     const menu = document.getElementById('mobile-menu');
     menu.classList.toggle('hidden');
 });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3, // Exibe 3 slides ao mesmo tempo
+    spaceBetween: 30, // Espaçamento entre os slides
+    loop: true, // Ativa a rolagem infinita
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true, // Permite interação com as bolinhas
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
